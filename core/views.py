56b8798ceba3cc7d46971e58d11bb5afd7686125
SunnatilloQuantic <sunnatilloshavkatov@gmail.com>
2021-01-19 14:38:37 +0500

    update

--- core/views.py
+++ core/views.py
@@ -1,10 +1,7 @@
-from django.contrib.auth.models import User
-# third party imports
 from rest_framework import mixins, generics, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import generics
 
 from .register import RegisterSerializer, UserSerializer
 
@@ -14,8 +11,6 @@ from .models import Post
 
 
 class TestView(APIView):
-
-    permission_classes = (IsAuthenticated)
 
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
